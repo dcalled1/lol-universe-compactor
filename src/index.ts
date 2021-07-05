6b6568facedf0d6ccffdd0c9b7a4e4c3b5f42be9
David Calle <dcalled1@eafit.edu.co>
2021-07-05 09:33:19 -0500

    por fin algo de avance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("de vuelta al principio :´)")
+import puppeteer from 'puppeteer'
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://universe.leagueoflegends.com/en_US/story/zyra-color-story/');
+    await page.waitForSelector('div#Content');
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
+})();
