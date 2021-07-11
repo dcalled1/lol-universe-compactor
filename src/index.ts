47605773a2dac8e7e0eee5164706b461cc9147f9
David Calle <dcalled1@eafit.edu.co>
2021-07-11 10:06:37 -0500

    uwu

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import puppeteer from 'puppeteer'
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://universe.leagueoflegends.com/en_US/story/zyra-color-story/');
-    await page.waitForSelector('div#Content');
-    await page.screenshot({ path: 'example.png' });
+    await page.goto('https://universe.leagueoflegends.com/en_US/champions/');
+    await page.waitForSelector('article.championsPage_23K7 li.item_30l8');
+    let urls = await page.$$eval("li.item_30l8", links => {
+        return links.map(el => {
+            const t = {url: el.querySelector("a")!.href, name: el.querySelector("h1")?.textContent}
+            return t;
+        })
+    });
+    console.log(urls)
 
     await browser.close();
 })();
